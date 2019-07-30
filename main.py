9d9d260900c3bb64ee98cde6f22831eb5c02e483
Giuseppe Sorrentino <uhpnou@gmail.com>
2019-07-30 21:44:36 +0200

    refactor tests, refactor code, The Goose implemented

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import random
 import re
 
 CUBE_FACES = 6
+WIN_POSITION = 63
+BRIDGE_START = 6
+BRIDGE_ARRIVE = 12
+GOOSES = [5, 9, 14, 18, 23, 27]
 
 def print_players(players):
     print('players:', ', '.join(players))
@@ -16,43 +20,67 @@ def add_player(players, positions, not_won, result):
         print_players(players)
 
 
+
 def move_player(players, positions, not_won, result, rolling=False):
     try:
         index = players.index(result.group(1))
     except ValueError:
         print('The player', result.group(1), 'does not exist.')
     else:
-        cube_one = roll_dice(CUBE_FACES) if rolling else result.group(2)
-        cube_two = roll_dice(CUBE_FACES) if rolling else result.group(3)
-        steps = int(cube_one) + int(cube_two)
         old_position = positions[index]
-        positions[index] += int(steps)
+        cube_one, cube_two, steps = build_movements(result, rolling)
+        new_position = old_position + steps
+        move_player_to_position(index, positions, old_position + steps)
 
-        msg_components = [
-            players[index],
-            cube_one,
-            cube_two,
-            players[index],
-            'Start' if old_position == 0 else old_position,
-            positions[index],
-            players[index],
-        ]
+        msg_components = {
+            'name': players[index],
+            'cube_one': cube_one,
+            'cube_two': cube_two,
+            'old_position': 'Start' if old_position == 0 else old_position,
+            'new_position': positions[index],
+            'bridge_arrive': BRIDGE_ARRIVE
+        }
 
-        if positions[index] == 63:
-            print('{} rolls {}, {}. {} moves from {} to {}. {} Wins!!'.format(*msg_components))
+        if positions[index] == WIN_POSITION:
+            print('{name} rolls {cube_one}, {cube_two}. {name} moves from {old_position} to {new_position}. {name} Wins!!'.format(**msg_components))
             not_won[0] = False
+        elif positions[index] == BRIDGE_START:
+            move_player_to_position(index, positions, BRIDGE_ARRIVE)
+            print('{name} rolls {cube_one}, {cube_two}. {name} moves from {old_position} to The Bridge. {name} jumps to {bridge_arrive}'.format(**msg_components))
+        elif positions[index] in GOOSES:
+            print('{name} rolls {cube_one}, {cube_two}. {name} moves from {old_position} to {new_position}, The Goose. '.format(**msg_components),  end='')
+            move_player_gooses(index, positions, new_position, steps, players)
         else:
-            print('{} rolls {}, {}. {} moves from {} to {}'.format(*msg_components))
+            print('{name} rolls {cube_one}, {cube_two}. {name} moves from {old_position} to {new_position}'.format(**msg_components))
 
 def roll_dice(cube_faces):
     return random.randint(1, cube_faces)
 
+def build_movements(result, rolling):
+    cube_one = roll_dice(CUBE_FACES) if rolling else result.group(2)
+    cube_two = roll_dice(CUBE_FACES) if rolling else result.group(3)
+    steps = int(cube_one) + int(cube_two)
+
+    return cube_one, cube_two, steps
+
 def move_player_with_rolling(players, positions, not_won, result):
     move_player(players, positions, not_won, result, True)
 
+def move_player_to_position(index, positions, position):
+    positions[index] = position
+
+def move_player_gooses(index, positions, old_position, steps, players):
+    new_position = old_position + steps
+    if new_position in GOOSES:
+        print('{} moves again and goes to {}, The Goose. '.format(players[index], new_position), end='')
+        move_player_gooses(index, positions, new_position, steps, players)
+    else:
+        move_player_to_position(index, positions, new_position)
+        print('{} moves again and goes to {}'.format(players[index], new_position))
+
 REGEXES = [
     r'^add player (.*)$',
-    r'^move (\w*)(?!\d+, \d+)$',
+    r'^move (\w*)$',
     r'^move (\w*) (\d+), (\d+)$'
 ]
 
@@ -86,6 +114,8 @@ def main(commands_number=-1):
 
         if commands_number != -1:
             commands_number -= 1
+
+    return players, positions
 
 
 if __name__ == '__main__':
--- main_tests.py
+++ main_tests.py
@@ -11,31 +11,40 @@ class TestAdd(unittest.TestCase):
     def test_add_player(self):
         user_input = [
             'add player Giorgio',
-            'add player Giorgio'
+            'add player Giorgio',
+            'add player Franco',
+            'add player Franco'
         ]
 
-        expected_output = 'players: Giorgio\nGiorgio : already existing player'
+        expected_output = 'players: Giorgio\nGiorgio : already existing player\nplayers: Giorgio, Franco\nFranco : already existing player'
 
         with patch('builtins.input', side_effect=user_input), patch('sys.stdout', new_callable=StringIO) as out:
-            main.main(1)
+            players, positions = main.main(3)
 
         output = out.getvalue().strip()
         self.assertEqual(output, expected_output)
+        self.assertEqual(players, ['Giorgio', 'Franco'])
+        self.assertEqual(positions, [0, 0])
 
     def test_move_player(self):
         user_input = [
             'add player Giorgio',
             'move Giorgio 3, 4',
-            'move Giorgio 2, 2'
+            'move Giorgio 2, 2',
+            'add player Franco',
+            'move Franco 3, 4',
+            'move Franco 2, 2'
         ]
 
-        expected_output = 'players: Giorgio\nGiorgio rolls 3, 4. Giorgio moves from Start to 7\nGiorgio rolls 2, 2. Giorgio moves from 7 to 11'
+        expected_output = 'players: Giorgio\nGiorgio rolls 3, 4. Giorgio moves from Start to 7\nGiorgio rolls 2, 2. Giorgio moves from 7 to 11\nplayers: Giorgio, Franco\nFranco rolls 3, 4. Franco moves from Start to 7\nFranco rolls 2, 2. Franco moves from 7 to 11'
 
         with patch('builtins.input', side_effect=user_input), patch('sys.stdout', new_callable=StringIO) as out:
-            main.main(2)
+            players, positions = main.main(5)
 
         output = out.getvalue().strip()
         self.assertEqual(output, expected_output)
+        self.assertEqual(players, ['Giorgio', 'Franco'])
+        self.assertEqual(positions, [11, 11])
 
     def test_player_wins(self):
         user_input = [
@@ -46,11 +55,62 @@ class TestAdd(unittest.TestCase):
         expected_output = 'players: Giorgio\nGiorgio rolls 63, 0. Giorgio moves from Start to 63. Giorgio Wins!!'
 
         with patch('builtins.input', side_effect=user_input), patch('sys.stdout', new_callable=StringIO) as out:
-            main.main()
+            players, positions = main.main()
 
         output = out.getvalue().strip()
         self.assertEqual(output, expected_output)
+        self.assertEqual(players, ['Giorgio'])
+        self.assertEqual(positions, [63])
 
+    def test_the_bridge(self):
+        user_input = [
+            'add player Giorgio',
+            'move Giorgio 6, 0'
+        ]
+
+        expected_output = 'players: Giorgio\nGiorgio rolls 6, 0. Giorgio moves from Start to The Bridge. Giorgio jumps to 12'
+
+        with patch('builtins.input', side_effect=user_input), patch('sys.stdout', new_callable=StringIO) as out:
+            players, positions = main.main(1)
+
+        output = out.getvalue().strip()
+        self.assertEqual(output, expected_output)
+        self.assertEqual(players, ['Giorgio'])
+        self.assertEqual(positions, [12])
+
+    def test_the_goose_single(self):
+        user_input = [
+            'add player Giorgio',
+            'move Giorgio 3, 0',
+            'move Giorgio 1, 1'
+        ]
+
+        expected_output = 'players: Giorgio\nGiorgio rolls 3, 0. Giorgio moves from Start to 3\nGiorgio rolls 1, 1. Giorgio moves from 3 to 5, The Goose. Giorgio moves again and goes to 7'
+
+        with patch('builtins.input', side_effect=user_input), patch('sys.stdout', new_callable=StringIO) as out:
+            players, positions = main.main(2)
+
+        output = out.getvalue().strip()
+        self.assertEqual(output, expected_output)
+        self.assertEqual(players, ['Giorgio'])
+        self.assertEqual(positions, [7])
+
+    def test_the_goose_double(self):
+        user_input = [
+            'add player Giorgio',
+            'move Giorgio 10, 0',
+            'move Giorgio 2, 2'
+        ]
+
+        expected_output = 'players: Giorgio\nGiorgio rolls 10, 0. Giorgio moves from Start to 10\nGiorgio rolls 2, 2. Giorgio moves from 10 to 14, The Goose. Giorgio moves again and goes to 18, The Goose. Giorgio moves again and goes to 22'
+
+        with patch('builtins.input', side_effect=user_input), patch('sys.stdout', new_callable=StringIO) as out:
+            players, positions = main.main(2)
+
+        output = out.getvalue().strip()
+        self.assertEqual(output, expected_output)
+        self.assertEqual(players, ['Giorgio'])
+        self.assertEqual(positions, [22])
  
 if __name__ == '__main__':
     unittest.main()
